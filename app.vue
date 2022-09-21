79c8338887558eddf8e1c8b85dfef1d71588b9c1
Heurtault Aubin <aub.heurtault@gmail.com>
2022-09-21 18:38:07 +0200

    setup

--- app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="flex justify-center items-center h-screen">
-    <h1 class="text-3xl font-bold text-green-500">
-      Tailwind CSS 3 is installed in Nuxt 3
-    </h1>
-  </div>
-</template>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="app-layout">
+        <main>
+            <Navbar/>
+            <slot />
+        </main>
+    </div>
+  </template>
+  <script>
+    
+  </script>
